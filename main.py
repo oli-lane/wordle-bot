bee664182bb094a62adbbfb10c8c86df0957f367
oli-lane <markus.oliver.lane@gmail.com>
2022-06-27 10:40:59 -0700

    basic framework

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 def main():
     print("Hello World!")
 
+def solve():
+    valid_words = ALL_WORDS
+    while True:
+            guess = make_guess(valid_words)
+            print("Guess: " + guess.upper())
+            result = collect_result()
+            if result == CORRECT:
+                print("I won!")
+                break
+            valid_words = update_valid_words(valid_words, guess, result)
+
 
 if __name__ == "__main__":
     main()
