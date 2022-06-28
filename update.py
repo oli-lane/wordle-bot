1b88204523120074a251f7672f3de2180bfb158d
Nick Bircher <nicky.b.three@gmail.com>
2022-06-28 16:50:20 -0700

    Updated evaluate.py - Nick

---
+++ update.py
@@ -0,0 +1,9 @@
+
+
+def update(valid_words, guess):
+    """
+
+    :param valid_words: List? or .txt file? of valid words
+    :param guess:
+    :return: List? or .txt file? of updated valid words
+    """
