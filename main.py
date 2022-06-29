69f8a5ea56b56555083608ac449b6a728e977371
oli-lane <markus.oliver.lane@gmail.com>
2022-06-29 15:54:22 -0700

    method header tweaks

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import guess
 from evaluate import evaluate_word
 
 
-def main():
-  solve()
+def main(infile=None):
+  solve(infile)
 
 
-def solve():
+def solve(infile):
   num_guesses = 0
   with open('all_words.txt') as ifp:
     valid_words = list(map(lambda x: x.strip(), ifp.readlines()))
@@ -17,8 +17,8 @@ def solve():
     result = evaluate_word()  # 5 element array of nums 0, 1, or 2
     if num_guesses > 6:
       print("fail")
-      break
-    if result == CORRECT:
+    # if result is correct
+    if result == [2, 2, 2, 2, 2]:
       print("success")
       break
     valid_words = update_valid_words(valid_words, guess, result)
