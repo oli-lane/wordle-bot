4d69208856a79780aea9d1a27d7db9d950c59c9a
oli-lane <markus.oliver.lane@gmail.com>
2022-07-04 13:52:46 -0700

    main tweaks to return num of guesses, test harness first draft

--- main.py
+++ main.py
@@ -6,24 +6,25 @@ from update import update_words
 def main():
   print("Wordle Bot\n")
   word = input("Enter a word: ")
-  solve(word)
+  num = solve(word, "yes")
+  print("Your word took " + str(num) + " guesses to solve!\n")
 
 
-def solve(word):
+def solve(word, verbosity) -> int:
   num_guesses = 0
   with open('all_words.txt') as ifp:
     valid_words = list(map(lambda x: x.strip(), ifp.readlines()))
   while True:
     cur_guess = guess.guess_word_2(valid_words)
     num_guesses += 1
-    print("Guess: " + cur_guess.upper())
+    if verbosity == "yes":
+      print("Guess " + str(num_guesses) + " : " + cur_guess.upper())
     result = evaluate_word()  # 5 element array of nums 0, 1, or 2
     if num_guesses > 6:
       print("fail")
     # if result is correct
     if result == [2, 2, 2, 2, 2]:
-      print("Your word took " + str(num_guesses) + " to solve!\n")
-      break
+      return num_guesses
     valid_words = update_words(valid_words, guess, result)
 
 
