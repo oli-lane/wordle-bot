306ca3ad63ce8a0d7eec9152ebe5bc0555e29a6b
Nick Bircher <nicky.b.three@gmail.com>
2022-07-05 09:44:54 -0700

    Update_words() + various tweaks - Nick

--- main.py
+++ main.py
@@ -4,27 +4,27 @@ from update import update_words
 
 
 def main():
-  print("Wordle Bot\n")
-  word = input("Enter a word: ")
-  num = solve(word, "yes")
-  print("The Wordle bot solved " + word + " in " + str(num) + " guesses!")
+    print("Wordle Bot\n")
+    word = input("Enter a word: ")
+    num = solve(word, "yes")
+    print("The Wordle bot solved " + word + " in " + str(num) + " guesses!")
 
 
 def solve(word, verbosity) -> int:
-  num_guesses = 0
-  with open('all_words.txt') as ifp:
-    valid_words = list(map(lambda x: x.strip(), ifp.readlines()))
-  while True:
-    cur_guess = guess.guess_word_2(valid_words)
-    num_guesses += 1
-    if verbosity == "yes":
-      print("Guess " + str(num_guesses) + " : " + cur_guess.upper())
-    result = evaluate_word(cur_guess, word)  # 5 element array of nums 0, 1, or 2
-    # if result is correct
-    if result == [2, 2, 2, 2, 2]:
-      return num_guesses
-    valid_words = update_words(valid_words, guess, result)
+    num_guesses = 0
+    with open('all_words.txt') as ifp:
+        valid_words = list(map(lambda x: x.strip(), ifp.readlines()))
+    while True:
+        cur_guess = guess.guess_word_2(valid_words)
+        num_guesses += 1
+        if verbosity == "yes":
+            print("Guess " + str(num_guesses) + " : " + cur_guess.upper())
+        result = evaluate_word(cur_guess, word)  # 5 element array of nums 0, 1, or 2
+        # if result is correct
+        if result == [2, 2, 2, 2, 2]:
+            return num_guesses
+        valid_words = update_words(valid_words, guess, result)
 
 
 if __name__ == "__main__":
-  main()
+    main()
--- update.py
+++ update.py
@@ -1,9 +1,18 @@
 from evaluate import *
 
+
 def update_words(valid_words, guess, result):
     """
-    
-    :param valid_words: List? or .txt file? of valid words
-    :param guess: 
-    :return: List? or .txt file? of updated valid words 
+    removes words that can't be guessed based on result
+    simple O(n+m^2) solution
+    :param valid_words: List of valid words
+    :param guess: 5 char word
+    :param result: 5 element list indicating correctness of guess
+    :return: List of updated valid words
     """
+    for word in valid_words:
+        if evaluate_word(guess, word) != result:
+            valid_words.remove(word)
+
+    return valid_words
+
